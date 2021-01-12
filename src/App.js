52e133c20e2c843e258608fdc8bca9ec1521c0b2
Asmita Sharma <asmitasharma@Asmitas-MacBook-Pro.local>
2021-01-12 07:12:17 +0530

    Created a form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
 
 class App extends Component {
 
@@ -7,32 +8,18 @@ class App extends Component {
     const { characters } = this.state;
     
     return (
+        
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form />
       </div>
     );
   }
   
     state = {
-      characters : [
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            name: 'Dee',
-            job: 'Aspring actress',
-        },
-        {
-            name: 'Dennis',
-            job: 'Bartender',
-        },
-      ]
+      characters : []
     }
+    
   
 
   removeCharacter = index => {
@@ -44,9 +31,7 @@ class App extends Component {
       }),
     })
   }
-
 }
-
 
 
 export default App
